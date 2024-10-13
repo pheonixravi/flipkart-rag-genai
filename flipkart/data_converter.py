50409fc143ccabbfea82056237949c79bda63727
scott <scott@gmail.com>
2024-10-13 19:54:07 +0530

    implemented data_converter.py file

--- flipkart/data_converter.py
+++ flipkart/data_converter.py
@@ -2,7 +2,6 @@ import pandas as pd
 from langchain_core.documents import Document
 
 def dataconverter():
-
     product_data = pd.read_csv(r"data\flipkart_product_review.csv")
 
     data = product_data[["product_title", "review"]]
